b3f8d8454a3bd7a21399a68602c3952dbd9754af
flitzi <thomas.gocke@gmx.de>
2015-02-01 20:32:53 +0100

    AC_TrackCycle 1.0.0

---
+++ server/AC_TrackCycle/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace AC_TrackCycle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonNextTrack = new System.Windows.Forms.Button();
+            this.textBoxCurrentCycle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxAutoChangeTrack = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonStart.Location = new System.Drawing.Point(218, 580);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 38);
+            this.textBoxOutput.MaxLength = 327670000;
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxOutput.Size = new System.Drawing.Size(490, 536);
+            this.textBoxOutput.TabIndex = 1;
+            this.textBoxOutput.WordWrap = false;
+            // 
+            // buttonNextTrack
+            // 
+            this.buttonNextTrack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonNextTrack.Location = new System.Drawing.Point(427, 580);
+            this.buttonNextTrack.Name = "buttonNextTrack";
+            this.buttonNextTrack.Size = new System.Drawing.Size(75, 23);
+            this.buttonNextTrack.TabIndex = 2;
+            this.buttonNextTrack.Text = "Next Track";
+            this.buttonNextTrack.UseVisualStyleBackColor = true;
+            this.buttonNextTrack.Click += new System.EventHandler(this.buttonNextTrack_Click);
+            // 
+            // textBoxCurrentCycle
+            // 
+            this.textBoxCurrentCycle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBoxCurrentCycle.Location = new System.Drawing.Point(138, 12);
+            this.textBoxCurrentCycle.Name = "textBoxCurrentCycle";
+            this.textBoxCurrentCycle.ReadOnly = true;
+            this.textBoxCurrentCycle.Size = new System.Drawing.Size(135, 20);
+            this.textBoxCurrentCycle.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Currently running:";
+            // 
+            // checkBoxAutoChangeTrack
+            // 
+            this.checkBoxAutoChangeTrack.AutoSize = true;
+            this.checkBoxAutoChangeTrack.Checked = true;
+            this.checkBoxAutoChangeTrack.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAutoChangeTrack.Location = new System.Drawing.Point(12, 584);
+            this.checkBoxAutoChangeTrack.Name = "checkBoxAutoChangeTrack";
+            this.checkBoxAutoChangeTrack.Size = new System.Drawing.Size(138, 17);
+            this.checkBoxAutoChangeTrack.TabIndex = 5;
+            this.checkBoxAutoChangeTrack.Text = "Change track after race";
+            this.checkBoxAutoChangeTrack.UseVisualStyleBackColor = true;
+            this.checkBoxAutoChangeTrack.CheckedChanged += new System.EventHandler(this.checkBoxAutoChangeTrack_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 615);
+            this.Controls.Add(this.checkBoxAutoChangeTrack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCurrentCycle);
+            this.Controls.Add(this.buttonNextTrack);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "AC Track Cycle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonNextTrack;
+        private System.Windows.Forms.TextBox textBoxCurrentCycle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxAutoChangeTrack;
+    }
+}
+
---
+++ server/AC_TrackCycle/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AC_TrackCycle
+{
+    public partial class Form1 : Form
+    {
+        private string serverfolder = @"D:\Games\Steam\SteamApps\common\assettocorsa\server\"; //not used if acServer.exe is found next to this app
+        private List<string> iniLines = new List<string>();
+        private List<Tuple<string, int>> tracks = new List<Tuple<string, int>>();
+        //private List<string> admins = new List<string>(); //not used, you have to pass the admin password everytime for /next_track and /change_track, e.g. "/next_track <mypassword>" or /change_track <mypassword> spa
+        private string next_track, change_track;
+
+        private int cycle;
+        Process serverInstance;
+
+        delegate void outputRecieved(string output);
+        delegate void clearOutput();
+
+        private bool autoChangeTrack = true;
+
+        public Form1()
+        {
+            this.InitializeComponent();
+
+            if (File.Exists(Path.Combine(Application.StartupPath, "acServer.exe")))
+            {
+                this.serverfolder = Application.StartupPath;
+            }
+
+            StreamReader sr = new StreamReader(Path.Combine(serverfolder, @"cfg\server_cfg.ini"));
+
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                if (line.StartsWith("TRACKS=", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    foreach (string parts in line.Substring(line.IndexOf("=") + 1).Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        string[] part = parts.Split(',');
+                        string track = part[0].Trim();
+                        int laps = int.Parse(part[1]);
+                        this.tracks.Add(new Tuple<string, int>(track, laps));
+                    }
+                }
+
+                if (line.StartsWith("ADMIN_PASSWORD=", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    string adminpw = line.Substring(line.IndexOf("=") + 1);
+                    this.next_track = "ADMIN COMMAND: /next_track " + adminpw;
+                    this.change_track = "ADMIN COMMAND: /change_track " + adminpw;
+                }
+
+                this.iniLines.Add(line);
+
+                this.textBoxOutput.AppendText(line + Environment.NewLine);
+
+                line = sr.ReadLine();
+            }
+
+            sr.Dispose();
+
+            if (this.tracks.Count == 0)
+            {
+                MessageBox.Show("No TRACKS in TRACK_CYCLE section found.");
+                Application.Exit();
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            if (this.serverInstance != null)
+            {
+                this.serverInstance.Kill();
+            }
+        }
+
+        private void writeOuput(string output)
+        {
+            if (!string.IsNullOrEmpty(output) && !output.StartsWith("No car with address"))
+            {
+                this.textBoxOutput.AppendText(output + Environment.NewLine);
+                if (this.cycle < this.tracks.Count)
+                {
+                    this.textBoxCurrentCycle.Text = this.tracks[this.cycle].Item1 + " " + this.tracks[this.cycle].Item2 + " laps";
+                }
+            }
+        }
+
+        private void resetOutput()
+        {
+            this.textBoxOutput.Text = string.Empty;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            this.cycle = 0;
+            this.StartServer();
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                this.resetOutput();
+
+                if (this.serverInstance != null)
+                {
+                    this.serverInstance.Kill();
+                }
+
+                if (this.cycle >= this.tracks.Count)
+                {
+                    this.cycle = 0;
+                }
+
+                StreamWriter sw = new StreamWriter(Path.Combine(this.serverfolder, @"cfg\server_cfg.ini"));
+                foreach (string line in this.iniLines)
+                {
+                    string outLine = line;
+
+                    if (line.StartsWith("TRACK=", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        outLine = "TRACK=" + this.tracks[cycle].Item1;
+                    }
+
+                    if (line.StartsWith("LAPS=", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        outLine = "LAPS=" + this.tracks[cycle].Item2;
+                    }
+
+                    sw.WriteLine(outLine);
+                }
+
+                sw.Dispose();
+
+                this.serverInstance = new Process();
+                this.serverInstance.StartInfo.FileName = Path.Combine(serverfolder, "acServer.exe");
+                this.serverInstance.StartInfo.WorkingDirectory = serverfolder;
+                this.serverInstance.StartInfo.RedirectStandardOutput = true;
+                this.serverInstance.StartInfo.UseShellExecute = false;
+                this.serverInstance.StartInfo.CreateNoWindow = true;
+                this.serverInstance.OutputDataReceived += process_OutputDataReceived;
+                this.serverInstance.Start();
+                this.serverInstance.BeginOutputReadLine();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " " + ex.StackTrace);
+                this.Close();
+            }
+        }
+
+        void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(e.Data))
+                {
+                    return;
+                }
+
+                this.BeginInvoke(new outputRecieved(this.writeOuput), e.Data);
+
+                if (this.autoChangeTrack&& e.Data == "HasSentRaceoverPacket, move to the next session")
+                {
+                    this.cycle++;
+                    this.StartServer();
+                }
+
+                //this is not secure, someone with the same name can exploit admin rights
+                //if (e.Data.StartsWith("Making") && e.Data.EndsWith("admin"))
+                //{
+                //    admins.Add(e.Data.Replace("Making", "").Replace("admin", "").Trim());
+                //}
+
+                //if (e.Data.StartsWith("ADMIN COMMAND: /next_track received from") && admins.Contains(e.Data.Replace("ADMIN COMMAND: /next_track received from", "").Trim()))
+                //{
+                //    cycle++;
+                //    StartServer();
+                //}
+
+                if (e.Data.StartsWith(this.next_track))
+                {
+                    this.cycle++;
+                    this.StartServer();
+                }
+
+                if (e.Data.StartsWith(this.change_track))
+                {
+                    string track = e.Data.Substring(this.change_track.Length).Trim();
+                    track = track.Substring(0, track.IndexOf(" "));
+                    for (int i = 0; i < this.tracks.Count; i++)
+                    {
+                        if (this.tracks[i].Item1 == track)
+                        {
+                            this.cycle = i;
+                            this.StartServer();
+                            break;
+                        }
+                    }                   
+                }
+            }
+            catch (Exception ex)
+            {
+                this.BeginInvoke(new outputRecieved(this.writeOuput), ex.Message + " " + ex.StackTrace);
+            }
+        }
+
+        private void buttonNextTrack_Click(object sender, EventArgs e)
+        {
+            this.cycle++;
+            this.StartServer();
+        }
+
+        private void checkBoxAutoChangeTrack_CheckedChanged(object sender, EventArgs e)
+        {
+            this.autoChangeTrack = this.checkBoxAutoChangeTrack.Checked;
+        }
+    }
+}
